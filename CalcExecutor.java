6664ceae8a80f56e0014ac7cc546a23187722e7f
AayushPalak <apalak994@gmail.com>
2022-02-22 13:06:19 +0530

    Initial version of Calculator

---
+++ CalcExecutor.java
@@ -0,0 +1,12 @@
+
+public class CalcExecutor {
+
+	public static void main(String[] args) {
+		
+		Calculator c = new Calculator();
+		System.out.println("Addition result is:" + c.add(4, 5));
+		System.out.println("Subtraction result is:" + c.sub(9, 5));
+		
+	}
+
+}
